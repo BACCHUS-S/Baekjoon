dfecd0eae91690fb9b581e757795738a50d2b085
SungHyeon Bae <37185394+CyclamenRepandum@users.noreply.github.com>
2018-04-14 09:56:46 +0900

    Sort

--- 10989.py
+++ 10989.py
@@ -2,7 +2,7 @@ import sys
 input = sys.stdin.readline
 list=[0 for _ in range(10001)]
 for _ in range(int(input())):
-    list[int(int(input()))] += 1
+    list[int(input())] += 1
 for _ in range(10001):
     if list[_]:
          print('%s\n'%_*list[_],end='')
